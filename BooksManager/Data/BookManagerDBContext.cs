11cc2c8dcd4885dc93eeadacd0bf66d66c7327be
Diego Briones <dbrionesmtz@yahoo.com>
2022-02-04 10:46:44 -0600

    Configurando la conexion con la Base de Datos

---
+++ BooksManager/Data/BookManagerDBContext.cs
@@ -0,0 +1,25 @@
+﻿using BooksManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksManager.Data
+{
+    public class BookManagerDBContext: DbContext
+    {
+        public BookManagerDBContext(DbContextOptions<BookManagerDBContext> options) : base(options) 
+        {
+            //
+        }
+
+        public DbSet<Book> books { get; set; }
+        public DbSet<Author> author { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Publisher> publisher { get; set; }
+
+        /*
+         Migraciones
+        - agregar una migracion << add-migration NameOfMigration >>
+
+
+         */
+    }
+}
