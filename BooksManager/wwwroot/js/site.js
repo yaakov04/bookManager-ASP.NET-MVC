c6c63f45aaa2c0348b399e61d60b148713c29328
Diego Briones <dbrionesmtz@yahoo.com>
2022-02-08 16:03:41 -0600

    Ajustes para actualizar la fila en la vista

--- BooksManager/wwwroot/js/site.js
+++ BooksManager/wwwroot/js/site.js
@@ -91,13 +91,24 @@ function updateRow(modal, btn){
       Name:  modal.find('input[name="Name"]')[0] || null,
       LastName: modal.find('input[name="LastName"]')[0] || null,
       Country: modal.find('input[name="Country"]')[0] || null,
-  };
- 
+      Title: modal.find('input[name="Title"]')[0] || null,
+      PublishedYear: modal.find('input[name="PublishedYear"]')[0] || null,
 
+      AuthorId: $(modal.find('select[name="AuthorId"]')[0]).find('option:selected')[0] || null,
+      PublisherId: $(modal.find('select[name="PublisherId"]')[0]).find('option:selected')[0] || null,
+      CategoryId: $(modal.find('select[name="CategoryId"]')[0]).find('option:selected')[0] || null,
+  };
+    
   const newValues = {
       name: modalInput.Name ? modalInput.Name.value : null ,
       lastName: modalInput.LastName ? modalInput.LastName.value : null,
       country: modalInput.Country ? modalInput.Country.value : null,
+      title: modalInput.Title ? modalInput.Title.value : null,
+      year: modalInput.PublishedYear ? modalInput.PublishedYear.value : null,
+
+      author: modalInput.AuthorId || null,
+      publisher: modalInput.PublisherId || null,
+      category: modalInput.CategoryId || null,
     };
 
     const row = btn.parentElement.parentElement.parentElement;
@@ -106,22 +117,68 @@ function updateRow(modal, btn){
         name: $(row).find('td.name')[0] || null,
         lastname: $(row).find('td.lastname')[0] || null,
         country: $(row).find('td.country')[0] || null,
+        title: $(row).find('td.title')[0] || null,
+        year: $(row).find('td.year')[0] || null,
+
+        author: $(row).find('td.author')[0] || null,
+        publisher: $(row).find('td.publisher')[0] || null,
+        category: $(row).find('td.category')[0] || null,
     };
 
     if(td.name){td.name.innerHTML = newValues.name}
     if (td.lastname) { td.lastname.innerHTML = newValues.lastName }
     if (td.country) { td.country.innerHTML = newValues.country }
+    if (td.title) { td.title.innerHTML = newValues.title }
+    if (td.year) { td.year.innerHTML = newValues.year }
+
+    if (td.author) { td.author.innerHTML = newValues.author.innerText }
+    if (td.publisher) { td.publisher.innerHTML = newValues.publisher.innerText }
+    if (td.category) { td.category.innerHTML = newValues.category.innerText }
 
     const form = btn.parentElement;
     const inputs ={
         Name: $(form).find('input[name="Name"]') || null,
         LastName: $(form).find('input[name="LastName"]') || null,
         Country: $(form).find('input[name="Country"]') || null,
+        Title: $(form).find('input[name="Title"]') || null,
+        PublishedYear: $(form).find('input[name="PublishedYear"]') || null,
+
+        AuthorId: $(form).find('select[name="AuthorId"]')[0] || null,
+        PublisherId: $(form).find('select[name="PublisherId"]')[0] || null,
+        CategoryId: $(form).find('select[name="CategoryId"]')[0] || null,
     };
 
     if(inputs.Name){inputs.Name.value = newValues.name}
     if (inputs.LastName) { inputs.LastName.value = newValues.lastName }
     if (inputs.Country) { inputs.Country.value = newValues.country }
+    if (inputs.Title) { inputs.Title.value = newValues.title }
+    if (inputs.PublishedYear) { inputs.PublishedYear.value = newValues.year }
+
+    if (inputs.AuthorId) {
+        const opt = $(inputs.AuthorId).find('option')
+        for (let i = 0; i < opt.length; i++) {
+            $(opt[i]).removeAttr("selected")
+        }
+        $(inputs.AuthorId).find(`option[value = ${newValues.author.value}]`).attr('selected', 'selected');
+    }
+
+    if (inputs.PublisherId) {
+        const opt = $(inputs.PublisherId).find('option')
+        for (let i = 0; i < opt.length; i++) {
+            $(opt[i]).removeAttr("selected")
+        }
+        $(inputs.PublisherId).find(`option[value = ${newValues.publisher.value}]`).attr('selected', 'selected');
+    }
+
+    if (inputs.CategoryId) {
+        const opt = $(inputs.CategoryId).find('option')
+        for (let i = 0; i < opt.length; i++) {
+            $(opt[i]).removeAttr("selected")
+        }
+        $(inputs.CategoryId).find(`option[value = ${newValues.category.value}]`).attr('selected', 'selected');
+    }
+
+    console.log(inputs.AuthorId);
     
 }
 
